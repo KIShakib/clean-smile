1af707e5e0292265c53496cab7d87b9721b63bb2
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-11-22 23:03:37 +0600

    Every Dependency Are Installed

--- src/Pages/DummyPage/DummyPage.js
+++ src/Pages/DummyPage/DummyPage.js
@@ -3,7 +3,6 @@ import React from 'react';
 const DummyPage = () => {
     return (
         <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit doloribus minus ipsam sed vel quod deleniti delectus temporibus maxime unde impedit dolores, tempora dolore culpa aliquid eos. Sequi perferendis incidunt voluptatibus est quibusdam tempora laudantium, voluptates pariatur blanditiis illum vitae facere, totam corrupti voluptatum alias dolores tenetur nostrum? Ipsa, aliquam.</p>
         </div>
     );
 };
